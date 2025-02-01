a4f48f409098847f90401320b020ffa5daed6186
raztz15 <raztz15@gmail.com>
2025-02-01 18:30:40 +0200

    Removing code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
 import { Suspense } from 'react';
 import AppRoutes from './routes';
-import { Box } from '@mui/material';
 
 export default function App() {
   return (
     <Suspense fallback={'Loading...'}>
-      <Box sx={{ direction: 'rtl' }}>
-        <AppRoutes />
-      </Box>
+      <AppRoutes />
     </Suspense>
   );
 }
